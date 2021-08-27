1116f1626fb8db63eb3993a9c8cb7bcfec95f5cf
Vivek Patel <vivpatel99@gmail.com>
2021-08-27 19:50:27 +0200

    homeview model done

--- lib/pages/tabs/buy_tab_view.dart
+++ lib/pages/tabs/buy_tab_view.dart
@@ -8,39 +8,18 @@ import 'package:stacked/stacked.dart';
 
 class BuyTabView extends StatelessWidget {
   BuyTabView({Key? key}) : super(key: key);
-  final log = getLogger('BuyView');
+  final log = getLogger('BuyTabView');
   final catagoryItemsViewModel = locator<CatagoryItemsViewModel>();
   @override
   Widget build(BuildContext context) {
-    //todo add Stream view model
     log.d('------------------------  BuyTabView ------------------------');
     return ViewModelBuilder<TabViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
-          body:
-              DisplayNestedListView(groceryList: model.data, onBuyPage: true)),
+          body: model.isBusy
+              ? const CircularProgressIndicator()
+              : DisplayNestedListView(
+                  groceryList: model.data, onBuyPage: true)),
       viewModelBuilder: () => TabViewModel(),
     );
   }
 }
-//     return ViewModelBuilder<TabViewModel>.reactive(
-//       builder: (context, model, child) => Scaffold(
-//         body: StreamBuilder<Map<String, dynamic>?>(
-//           stream: model.stream,
-//           builder: (BuildContext context,
-//               AsyncSnapshot<Map<String, dynamic>?> snapshot) {
-//             if (snapshot.hasError) {
-//               log.e('StreamBuilder error ${snapshot.error}');
-//               return const SizedBox();
-//             } else {
-//               log.d('snapshot.data ${snapshot.data}');
-//               catagoryItemsViewModel.myGroceryList = snapshot.data;
-//               return DisplayNestedListView(
-//                   groceryList: snapshot.data, onBuyPage: true);
-//             }
-//           },
-//         ),
-//       ),
-//       viewModelBuilder: () => TabViewModel(),
-//     );
-//   }
-// }
